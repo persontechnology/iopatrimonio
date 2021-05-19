0d2cfd93bca00ecba29ec2d9ad6d2cab5f23b90a
persontechnology <persontechnologys@gmail.com>
2021-05-19 18:13:57 -0500

    info

--- resources/views/estaticas/arquitecturaReligiosa.blade.php
+++ resources/views/estaticas/arquitecturaReligiosa.blade.php
@@ -2,6 +2,14 @@
 
 @section('content')
 
+@if (!$iglesias->count()>0)
+    <div class="container">
+        <div class="alert alert-primary" role="alert">
+            <strong>No existe información para {{ $tipo }}</strong>
+        </div>
+    </div>
+@endif
+
 @include('estaticas.menuIglesias',['iglesias'=>$iglesias,'tipo'=>$tipo])
 
 @endsection
